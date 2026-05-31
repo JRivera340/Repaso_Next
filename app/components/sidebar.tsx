93fe63e7544ccb37590078affe35ea0991a1063c
JRivera340 <Riveragonzalezjoshua404@gmail.com>
2026-05-31 16:43:18 -0500

    Login terminado

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useAuthStore } from "../_store/auth.store"
+import Link from "next/link"
+
+export default function Sidebar(){
+
+    // extraemos user y logout del store global de Zustand
+    // user → para mostrar el nombre del usuario logueado
+    // logout → para cerrar sesión al hacer clic en el botón
+    const {user, logout} = useAuthStore()
+
+
+
+
+
+    return(
+        <div className="bg-gray-900 h-screen w-64 text-white p-6 flex flex-col">
+            <div>
+
+                <h1 className="text-2xl font-bold mb-1 ">Dash Peliculas</h1>
+                <p>Subtitulo</p>
+
+            </div>
+
+            <div>
+                <p>welcome</p>
+                <p>Usuario: {user?.name}</p>
+
+            </div>
+
+            <div>
+
+                <Link href={'/dashboard'} className="block py-2 hover:text-blue-400">Inico</Link>
+                <Link href={'/dashboard/peliculas'} className="block py-2 hover:text-blue-400">peliculas</Link>
+                <Link href={'/dashboard/counter'} className="block py-2 hover:text-blue-400">Contador</Link>
+            </div>
+
+            <button onClick={logout} className="mt-auto bg-red-500 py-2 px-4 rounded w-full">Cerrar Sesion</button>
+        </div>
+    )
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,32 @@
+'use client'
 //este layout es para esta url de dashboard
+import Sidebar from '@/app/components/sidebar'
+import { useAuthStore } from '@/app/_store/auth.store'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+
 
 export default function DashboardLayout({ children }: Readonly<{children : React.ReactNode}>) {
 
+     // guard — verificar si está autenticado
+    const { isAuthenticated } = useAuthStore()
+    const router = useRouter()
+
+    useEffect(() => {
+        // si no está autenticado, redirige al login
+        if (!isAuthenticated) {
+            router.push('/login')
+        }
+    }, [isAuthenticated])
+
     return(
 
-        <div>
-            <aside>
-
+        <div className='flex'>
+            <aside >
+            <Sidebar />
             </aside>
-            {children}
+            <main className="flex-1 p-8">{children}</main>
         </div>
         
     );
